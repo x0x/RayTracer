695c7d08f6ee2413da081104fd484e298d42eefb
nam3an2 <nam3an2@gmail.com>
2023-10-01 16:57:31 +0530

    add zoom and camera orientation feature

--- src/main.cpp
+++ src/main.cpp
@@ -18,20 +18,25 @@ int main() {
 
   HittableList world;
   auto material_ground = make_shared<Lambertian>(Color(0.2, 0.1, 0.9));
-  auto material_center = make_shared<Lambertian>(Color(0.1,0.2,0.5));
-  auto material_left   = make_shared<Dielectric>(1.5);
-  auto material_right  = make_shared<Metal>(Color(0.33, 0.12,0.01),1.0);
+  auto material_center = make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
+  auto material_left = make_shared<Metal>(Color(1,0,0),0);
+  auto material_right = make_shared<Metal>(Color(0.33, 0.12, 0.01), 1.0);
 
-  world.add(make_shared<Sphere>(Point( 0.0, -100.5, -1.0), 100.0, material_ground));
-world.add(make_shared<Sphere>(Point( 0.0,    0.0, -1.0),   0.5, material_center));
-world.add(make_shared<Sphere>(Point(-1.0,    0.0, -1.0),   0.5, material_left));
-world.add(make_shared<Sphere>(Point(-1.0,    0.0, -1.0),  -0.4, material_left));
-world.add(make_shared<Sphere>(Point( 1.0,    0.0, -1.0),   0.5, material_right));
+  world.add(
+      make_shared<Sphere>(Point(0.0, -100.5, -1.0), 100.0, material_ground));
+  world.add(make_shared<Sphere>(Point(0.0, 0.0, -1.0), 0.5, material_center));
+  world.add(make_shared<Sphere>(Point(-1.0, 0.0, -1.0), 0.5, material_left));
+  world.add(make_shared<Sphere>(Point(-1.0, 0.0, -1.0), -0.4, material_left));
+  world.add(make_shared<Sphere>(Point(1.0, 0.0, -1.0), 0.5, material_right));
 
   Camera camera;
-  camera.aspect_ratio_ = 1.0;
-  camera.image_width_ = 520;
-  camera.samples_per_pixel_ = 90;
-  camera.max_depth_ = 50;
+  camera.aspect_ratio_ = 1; 
+  camera.image_width_ = 800;
+  camera.samples_per_pixel_ = 200;
+  camera.max_depth_ = 8;
+  camera.fov_ = 80;      // vertical field of view in degrees
+
+  camera.lookFrom_ = Point(0,0,0);
+  camera.lookAt_ = Point(0,0,-1);
   camera.Render(world);
 }
